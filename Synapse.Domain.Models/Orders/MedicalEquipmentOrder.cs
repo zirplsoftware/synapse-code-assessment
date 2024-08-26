a41ddbaff2cb1663e2d8d9abefcf9b2829b77243
NATHANDESKTOP20\nlafratta <nlafratta@zirpl.com>
2024-08-25 23:28:42 -0400

    model changes

--- Synapse.Domain.Models/Orders/MedicalEquipmentOrder.cs
+++ Synapse.Domain.Models/Orders/MedicalEquipmentOrder.cs
@@ -2,8 +2,13 @@
 
 namespace Synapse.Domain.Models.Orders
 {
+    // NOTE: I only implemented the properties that were used in the original code
     public class MedicalEquipmentOrder
     {
+        // NOTE: due the original code implementation using JObject, 
+        // I don't know if this is actually an int or a string.
+        // going with assumption that it is an int.
+        // Unit tests will confirm.
         public int OrderId { get; set; }
         public IList<MedicalEquipmentOrderLineItem> Items { get; set; } = new List<MedicalEquipmentOrderLineItem>();
     }
--- Synapse.Domain.Models/Orders/MedicalEquipmentOrderLineItem.cs
+++ Synapse.Domain.Models/Orders/MedicalEquipmentOrderLineItem.cs
@@ -2,37 +2,28 @@
 
 namespace Synapse.Domain.Models.Orders
 {
+    // NOTE: I only implemented the properties that were used in the original code
     public class MedicalEquipmentOrderLineItem
     {
         private const string DeliveredStatus = "Delivered";
 
-        public int ItemId { get; set; }
         public string Description { get; set; }
 
-        // NOTE this should not be an int and is named poorly, 
-        // but this is the name and data type in original code
+        // NOTE: this is named poorly, 
+        // but this is the signature in original code
         // and I will make the assumption
         // that the code was correct in its
         // understanding of the API
-        public int? DeliveryNotification { get; set; }
+        public int DeliveryNotification { get; set; }
 
         // NOTE: this would be better as an enum, but
         // I will make the assumption that the called API 
         // is already defined
         public string Status { get; set; }
 
-        // NOTE: although not set up in this solution
-        // I treated this like I would an entity framework model relationship
-        // with a reference back to the principal entity
-        public int OrderId { get; set; }
-        public virtual MedicalEquipmentOrder Order { get; set; }
-
-
-
-
         // NOTE: I went with your OrdinalIgnoreCase, but I reviewed the difference recently
-        // between Ordinal and OrdinalIgnoreCase and I think InvariantIgnoreCase is typically more appropriate
-        // but I am keeping the original code
+        // between Ordinal and OrdinalIgnoreCase and I think InvariantIgnoreCase is typically
+        // more appropriate. I am keeping the original code.
         public bool IsDelivered => DeliveredStatus.Equals(Status, StringComparison.OrdinalIgnoreCase);
     }
 }
