1d9ec1400a93af6dee5db28ed58e978706b345f0
NATHANDESKTOP20\nlafratta <nlafratta@zirpl.com>
2024-08-26 00:03:37 -0400

    logging, notes, bug fix

--- Synapse.Domain.Services/Orders/OrderProcessingService.cs
+++ Synapse.Domain.Services/Orders/OrderProcessingService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Synapse.Domain.Models.Orders;
+using Synapse.Logging;
 
 namespace Synapse.Domain.Services.Orders
 {
@@ -20,7 +21,8 @@ namespace Synapse.Domain.Services.Orders
             }
             catch (Exception ex)
             {
-                // NATHAN: log
+                this.GetLog().Log("Failed to get orders", ex);
+
                 // we can eat this one as long as we log it.
                 // The API may be down and the next run of the
                 // job will try again.
@@ -37,27 +39,44 @@ namespace Synapse.Domain.Services.Orders
                         foreach (var item in order.Items)
                         {
                             // TODO: CHECK WITH PRODUCT OWNER: Duplicate delivery notifications
-                            // there is a bug in the original code - 
-                            // code that I would be unsure how to handle without
-                            // speaking with the product owner.
+                            // there is a bug in the original code that I would be unsure of
+                            // how to handle without speaking with the product owner.
                             // It appears to send the delivery alert every time
                             // the job runs once it is in a delivered status.
-                            // it keeps track of how many were sent, and updates
+                            // It keeps track of how many were sent, and updates
                             // that to the API, but there is no use of that data.
                             //
-                            // I will make this assumption since we want to go live
-                            // - We only want 1 delivery notification sent
+                            // I will make this assumption since we want to go live:
+                            // - We only want 1 delivery notification sent, ever
                             // 
-                            // Any bugs in my logic will need to be addressed
+                            // Any mistakes in my assumptions will need to be addressed
                             // next sprint or in a hotfix if critical
                             // 
                             if (item.IsDelivered
-                                && (item.DeliveryNotification == null
-                                    || item.DeliveryNotification.Value < 1))
+                                && item.DeliveryNotification < 1)
                             {
-                                await OrderService.SendOrderItemDeliveryAlertAsync(order.OrderId, item);
-                                item.DeliveryNotification = 1;
-                                requiresUpdate = true;
+                                try
+                                {
+                                    // this try/catch feels unnecessary, but I wanted
+                                    // to preserve the original code's behavior of logging 
+                                    // a success or a failure to send the alert.
+                                    // better approaches include specific exception types
+                                    // caught by the main try/catch block.
+                                    // this code results in a double log of the exception if it occurs.
+
+                                    await OrderService.SendOrderItemDeliveryAlertAsync(order.OrderId, item);
+                                    item.DeliveryNotification = 1;
+                                    requiresUpdate = true;
+
+                                    this.GetLog().Log($"Delivery alert sent for Order {order.OrderId}, Item: {item.Description}");
+                                }
+                                catch (Exception ex)
+                                {
+                                    this.GetLog().Log($"Send delivery alert failed for Order {order.OrderId}, Item: {item.Description}", ex);
+
+                                    // NOTE: use throw not rethrow
+                                    throw;
+                                }
                             }
                         }
 
@@ -66,14 +85,19 @@ namespace Synapse.Domain.Services.Orders
                             // TODO: an exception here requires thought on how to handle
                             // as we have already sent the notifications
                             // and will end up sending duplicate ones next time this runs
+                            // if we don't track that we have sent them somehow, despite the API being down.
                             await OrderService.UpdateMedicalEquipmentOrderAsync(order);
                         }
                     }
                     catch (Exception ex)
                     {
-                        // NOTE: Log, but move on to the next order so that the whole background job is not stopped
+                        // NOTE: in a real application, I would likely include the order JSON
+                        // in the message for debugging purposes using a JSON serializer
+                        // ex: var orderJson = JsonConvert.SerializeObject(order);
+                        // But I'll go simple here.
 
-                        // NATHAN: implement logging
+                        this.GetLog().Log($"Unexpected exception sending delivery updates for {order.OrderId}", ex);
+                        // NOTE: Log, but move on to the next order so that the whole background job is not stopped
                     }
                 }
             }
