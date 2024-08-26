c5ed43173fdd77d20e2f94b2fc5bf6c32bffc860
NATHANDESKTOP20\nlafratta <nlafratta@zirpl.com>
2024-08-25 23:35:50 -0400

    the program

--- Synapse.Applications.ProcessOrderUpdates/Program.cs
+++ Synapse.Applications.ProcessOrderUpdates/Program.cs
@@ -1,6 +1,19 @@
+using Synapse.Domain.Services.Orders;
+
+var orderProcessingService = new OrderProcessingService
+{
+    OrderService = new OrderService()
+};
+
 // NOTE: I personally prefer logging of exceptions to occur
 // at the highest level possible, which is usually before
-// they are eaten. Often but not always the entry point.
+// they are eaten. Often, but not always, the entry point.
 // In this case, logging is appearing throughout the code
 // since that approach requires more thoughtful design and more time
 // than an evaluation allows.
+//
+// This method logs and eats all exceptions.
+// This is why it is prefaced with Try.
+// Though it returns no bool indicating success
+// simply because there are no requirements for it.
+await orderProcessingService.TryProcessDeliveredOrderItemsAsync();
