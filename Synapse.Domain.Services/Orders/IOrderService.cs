8ae34b2758d8c65fffc7c02d029720ed32c03dbb
NATHANDESKTOP20\nlafratta <nlafratta@zirpl.com>
2024-08-25 22:41:21 -0400

    references and start of order service implementation

---
+++ Synapse.Domain.Services/Orders/IOrderService.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Synapse.Domain.Models.Orders;
+using Synapse.Net.Http;
+
+namespace Synapse.Domain.Services.Orders
+{
+    public interface IOrderService
+    {
+        Task<MedicalEquipmentOrder[]> GetMedicalEquipmentOrdersAsync();
+    }
+
+    public class OrderService : IOrderService
+    {
+        // NOTE: this would be retrieved from configuration
+        // of some sort in a real application rather than hardcoded
+        private const string OrdersUrl = "https://orders-api.com/orders";
+
+        public async Task<MedicalEquipmentOrder[]> GetMedicalEquipmentOrdersAsync()
+        {
+            using (var restClient = new RestClient())
+            {
+                // NOTE: the original code provided
+                // logged exceptions but did not propagate them.
+                // this is a bad idea.
+                // though sometimes it is desired, and in that case,
+                // a better method would be is async Task<bool> TryGetMedicalEquipmentOrdersOrdersAsync(out MedicalEquipmentOrder[] orders)
+                // but for my purposes, I am choosing to propagate the exception
+                // and let the caller handle it.
+                var orders = await restClient.GetAndParseResponseAsJsonAsync<MedicalEquipmentOrder[]>(OrdersUrl);
+                return orders;
+            }
+        }
+    }
+}
