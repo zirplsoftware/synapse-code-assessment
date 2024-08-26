5ed65e3e1a025f89ae95cd29284ec93d5ffee723
NATHANDESKTOP20\nlafratta <nlafratta@zirpl.com>
2024-08-25 22:08:09 -0400

    model classes

---
+++ Synapse.Domain.Models/Orders/MedicalEquipmentOrder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Synapse.Domain.Models.Orders
+{
+    public class MedicalEquipmentOrder
+    {
+        public int Id { get; set; }
+        public IList<MedicalEquipmentOrderLineItem> Items { get; set; } = new List<MedicalEquipmentOrderLineItem>();
+
+        // surely many more properties
+    }
+}
---
+++ Synapse.Domain.Models/Orders/MedicalEquipmentOrderLineItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Synapse.Domain.Models.Orders
+{
+    public class MedicalEquipmentOrderLineItem
+    {
+        public int Id { get; set; }
+        public MedicalEquipmentOrderStatusEnum Status { get; set; }
+
+        // NOTE: although not set up in this solution
+        // I treated this like I would an entity framework model relationship
+        // with a reference back to the principal entity
+        public int OrderId { get; set; }
+        public virtual MedicalEquipmentOrder Order { get; set; }
+        
+        // surely many more properties
+    }
+}
