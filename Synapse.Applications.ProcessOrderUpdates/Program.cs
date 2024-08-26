d899a518e475b4bee1fdfa2de49d217b956a45a9
NATHANDESKTOP20\nlafratta <nlafratta@zirpl.com>
2024-08-25 23:28:15 -0400

    note

---
+++ Synapse.Applications.ProcessOrderUpdates/Program.cs
@@ -0,0 +1,6 @@
+// NOTE: I personally prefer logging of exceptions to occur
+// at the highest level possible, which is usually before
+// they are eaten. Often but not always the entry point.
+// In this case, logging is appearing throughout the code
+// since that approach requires more thoughtful design and more time
+// than an evaluation allows.
